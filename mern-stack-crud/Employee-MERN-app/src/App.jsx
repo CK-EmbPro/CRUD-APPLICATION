3f6833168335b3e00678938cc37b0c68354f1e9d
CK-EmbPro <kennydebrice2@gmail.com>
2023-06-04 15:05:43 +0200

    This is Employee MERN app

---
+++ mern-stack-crud/Employee-MERN-app/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ChatEmp from './components/ChatEmp'
+
+function App() {
+
+  return (
+ <ChatEmp/>
+  )
+}
+
+export default App
